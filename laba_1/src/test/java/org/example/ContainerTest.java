852216556a1be96775d322f9558860a0af8c7d3d
LilyNik <lilyzazylil@gmail.com>
2024-10-25 20:40:49 +0300

    Добавлен javadoc, Update ContainerTest.java

--- laba_1/src/test/java/org/example/ContainerTest.java
+++ laba_1/src/test/java/org/example/ContainerTest.java
@@ -7,17 +7,27 @@ import junit.framework.TestSuite;
 public class ContainerTest
     extends TestCase
 {
-
-    public ContainerTest(String testName)
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ContainerTest(String testName )
     {
-        super(testName);
+        super( testName );
     }
 
+    /**
+     * @return the suite of tests being tested
+     */
     public static Test suite()
     {
-        return new TestSuite(ContainerTest.class);
+        return new TestSuite( ContainerTest.class );
     }
 
+    /**
+     * Rigourous Test :-)
+     */
     public void testApp()
     {
         assertTrue( true );
