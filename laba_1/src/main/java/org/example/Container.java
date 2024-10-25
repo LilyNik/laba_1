130e712b40903ece99359e9c216aa057b94a2f05
LilyNik <lilyzazylil@gmail.com>
2024-10-25 17:28:29 +0300

    Add files via upload

---
+++ laba_1/src/main/java/org/example/Container.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class Container
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
